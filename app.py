4307c1831a88f367114348b5f9957933cc281b02
fabio2000mig-dotcom <fabio2000.mig@gmail.com>
2026-04-25 11:27:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if not st.session_state.started:
  col1, col2 = st.columns([1, 5])
 
 with col1:
-    st.image("logo.png", width=70)
+    st.image("psf.png", width=70)
 
 with col2:
     st.markdown(
